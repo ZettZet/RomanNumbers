d63f0fe1013d091cb25b2bd361fb2c44b446fd13
Winkel <dmesser@yandex.ru>
2020-09-21 14:36:24 +0500

    rename func

--- tests/test_romenumbers.py
+++ tests/test_romenumbers.py
@@ -1,10 +1,10 @@
 import pytest
 
 
-from romannumbers import main
+from romannumbers import to_test
 from .conftest import generate_random_valid_roman_number, NUM_OF_TESTS
 
 
 @pytest.mark.parametrize("num", [generate_random_valid_roman_number() for _ in range(NUM_OF_TESTS)])
 def test_main(num: str):
-    assert num == main(num)
+    assert num == to_test(num)
