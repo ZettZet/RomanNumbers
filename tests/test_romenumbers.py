d99cd1545fa416731362e901d2da16321769e54c
mefolder <verybigfolder@yandex.ru>
2020-09-23 01:52:17 +0500

    Fix imports

--- tests/test_romenumbers.py
+++ tests/test_romenumbers.py
@@ -1,8 +1,6 @@
 import pytest
 
-
-from romannumbers import to_test
-from .conftest import generate_random_valid_roman_number, NUM_OF_TESTS
+from .conftest import generate_random_valid_roman_number, NUM_OF_TESTS, to_test
 
 
 @pytest.mark.parametrize("num", [generate_random_valid_roman_number() for _ in range(NUM_OF_TESTS)])
