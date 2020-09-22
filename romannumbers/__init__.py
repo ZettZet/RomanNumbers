1fd948cd76d0d341acfb9f93f70e49ca1b2505b3
mefolder <verybigfolder@yandex.ru>
2020-09-23 01:34:58 +0500

    _IS_ROMAN_REGEX is private and shouldn't be used outside module which it declared

--- romannumbers/__init__.py
+++ romannumbers/__init__.py
@@ -6,13 +6,18 @@ from typing import Dict, List
 _INTEGERS = (1000, 500, 100, 50, 10, 5, 1)
 _LITERALS = ('m', 'd', 'c', 'l', 'x', 'v', 'i')
 
-_REGEX: str = r'^(m{0,4})(d?c{0,3}|c[dm])(l?x{0,3}|x[lc])(v?i{0,3}|i[vx])$'
+_IS_ROMAN_REGEX = compile(r'^(m{0,4})(d?c{0,3}|c[dm])(l?x{0,3}|x[lc])(v?i{0,3}|i[vx])$', IGNORECASE)
 
 
 _FROM_ROMAN_NUMBERS: Dict[str, int] = {
     char: num for char, num in zip(_LITERALS, _INTEGERS)}
 _TO_ROMAN_NUMBERS: Dict[int, str] = {
     num: char for char, num in zip(_LITERALS, _INTEGERS)}
+
+
+def is_roman(string: str) -> bool:
+    "Check if given string is valid roman numeral"
+    return _IS_ROMAN_REGEX.search(string) is not None
 
 
 def arabic_to_roman(number: int) -> str:
@@ -111,7 +116,7 @@ def roman_to_arabic(string: str) -> int:
     if string == '':
         raise ValueError('Empty input string')
 
-    if not compile(_REGEX, IGNORECASE).search(string):
+    if not is_roman(string):
         raise ValueError('Inappropriate values')
 
     ints: List[int] = [_FROM_ROMAN_NUMBERS[item] for item in string.lower()]
@@ -124,7 +129,7 @@ def roman_to_arabic(string: str) -> int:
     return result
 
 
-__all__ = [arabic_to_roman, roman_to_arabic, _REGEX]
+__all__ = [arabic_to_roman, roman_to_arabic, is_roman]
 
 
 def to_test(roman: str) -> str:
@@ -138,7 +143,7 @@ if __name__ == '__main__':
     input_str: str = input('Roman or arabic number: ')
 
     try:
-        out = roman_to_arabic(input_str) if compile(_REGEX).search(
+        out = roman_to_arabic(input_str) if is_roman(
             input_str) else arabic_to_roman(int(input_str))
     except ValueError as ve:
         print(ve)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 from rstr import xeger
 
-from romannumbers import _REGEX
+from romannumbers import _IS_ROMAN_REGEX
 
 
 NUM_OF_TESTS = 1000
 
 
 def generate_random_valid_roman_number() -> str:
-    return xeger(_REGEX)
+    return xeger(_IS_ROMAN_REGEX)
